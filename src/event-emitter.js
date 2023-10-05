7063b4dd762dea11d8d4e34e35d5ffcaabcb9341
Can Acar <can.acar2@ogr.sakarya.edu.tr>
2023-10-05 11:31:52 +0300

    initial commit

--- src/event-emitter.js
+++ src/event-emitter.js
@@ -1,44 +1,44 @@
 class EventEmitter {
     constructor() {
-      this.events = new Map();
+        this.events = new Map();
     }
-  
+    
     on(eventName, listener) {
-      if (!this.events.has(eventName)) {
-        this.events.set(eventName, []);
-      }
-      this.events.get(eventName).push(listener);
-      return () => this.off(eventName, listener);
+        if (!this.events.has(eventName)) {
+            this.events.set(eventName, []);
+        }
+        this.events.get(eventName).push(listener);
+        return () => this.off(eventName, listener);
     }
-  
+    
     off(eventName, listener) {
-      if (this.events.has(eventName)) {
-        const listeners = this.events.get(eventName);
-        const index = listeners.indexOf(listener);
-        if (index !== -1) {
-          listeners.splice(index, 1);
+        if (this.events.has(eventName)) {
+            const listeners = this.events.get(eventName);
+            const index = listeners.indexOf(listener);
+            if (index !== -1) {
+                listeners.splice(index, 1);
+            }
+            if (listeners.length === 0) {
+                this.events.delete(eventName);
+            }
         }
-        if (listeners.length === 0) {
-          this.events.delete(eventName);
-        }
-      }
     }
-  
+    
     once(eventName, listener) {
-      const onceListener = (...args) => {
-        listener(...args);
-        this.off(eventName, onceListener);
-      };
-      this.on(eventName, onceListener);
+        const onceListener = (...args) => {
+            listener(...args);
+            this.off(eventName, onceListener);
+        };
+        this.on(eventName, onceListener);
     }
-  
+    
     emit(eventName, ...args) {
-      const eventListeners = this.events.get(eventName);
-      if (eventListeners) {
-        eventListeners.forEach((listener) => listener(...args));
-      }
+        const eventListeners = this.events.get(eventName);
+        if (eventListeners) {
+            eventListeners.forEach((listener) => listener(...args));
+        }
     }
-  }
-  
-  export default EventEmitter;
+}
+
+export default EventEmitter;
   
--- src/react-signal.js
+++ src/react-signal.js
@@ -1,91 +1,95 @@
-import { useEffect, useMemo, useState } from 'react';
+import {useEffect, useMemo, useState} from 'react';
 import EventEmitter from './event-emitter';
 
 class CreateSignal {
-  constructor(initialValue) {
-    this.emitter = new EventEmitter();
-    this.proxy = this.createProxy({ value: initialValue });
-  }
-
-  createProxy = (target) => {
-    const signal = this;
-    return new Proxy(target, {
-      get(target, property) {
-        const value = Reflect.get(...arguments);
-        if (typeof value === 'object' && value !== null) {
-          return signal.createProxy(value);
-        }
-        return value;
-      },
-
-      set(target, property, value) {
-        const oldValue = target[property];
-        target[property] = value;
-        if (oldValue !== value) {
-          signal.emitter.emit('valueChanged', {
-            property,
-            oldValue,
-            newValue: value,
-          });
-        }
-        return Reflect.set(...arguments);
-      },
-    });
-  };
-
-  get value() {
-    return this.proxy.value;
-  }
-
-  set value(newValue) {
-    this.proxy.value = newValue;
-  }
-  /* this is magic */
-  get useSignal() {
-    const signal = this;
-    const [_, forceUpdate] = useState();
-
-    useEffect(() => {
-      signal.emitter.on('valueChanged', ({ newValue }) => {
-        forceUpdate(newValue);
-      });
-    });
-
-    return useMemo(
-      () => () => {
-        return proxy;
-      },
-      [_]
-    );
-  }
-
-  onListen(listener) {
-    const signal = this;
-    signal.emitter.on('valueChanged', (data) => {
-      listener(data.property, data.oldValue, data.newValue);
-    });
-  }
-
-  subscribe(listener) {
-    return this.emitter.on('valueChanged', listener);
-  }
-
-  unsubscribe(listener) {
-    this.emitter.off('valueChanged', listener);
-  }
-
-  update(fn) {
-    const newValue = fn(this.value);
-    this.setValue(newValue);
-  }
-
-  setValue(newValue) {
-    if (this.value !== newValue) {
-      const oldValue = this.value;
-      this.value = newValue;
-      this.emitter.emit('valueChanged', { oldValue, newValue });
+    constructor(initialValue) {
+        this.emitter = new EventEmitter();
+        this.proxy = this.createProxy({value: initialValue});
     }
-  }
+    
+    createProxy = (target) => {
+        const signal = this;
+        return new Proxy(target, {
+            get(target, property) {
+                const value = Reflect.get(...arguments);
+                if (typeof value === 'object' && value !== null) {
+                    return signal.createProxy(value);
+                }
+                return value;
+            },
+            
+            set(target, property, value) {
+                const oldValue = target[property];
+                target[property] = value;
+                if (oldValue !== value) {
+                    signal.emitter.emit('valueChanged', {
+                        property,
+                        oldValue,
+                        newValue: value,
+                    });
+                }
+                return Reflect.set(...arguments);
+            },
+            has(target, p) {
+                return Reflect.has(...arguments);
+            }
+        });
+    };
+    
+    get value() {
+        return this.proxy.value;
+    }
+    
+    set value(newValue) {
+        this.proxy.value = newValue;
+    }
+    
+    /* this is magic */
+    get useSignal() {
+        const signal = this;
+        const [_, forceUpdate] = useState();
+        
+        useEffect(() => {
+            signal.emitter.on('valueChanged', ({newValue}) => {
+                forceUpdate(newValue);
+            });
+        });
+        
+        return useMemo(
+            () => () => {
+                return proxy;
+            },
+            [_]
+        );
+    }
+    
+    onListen(listener) {
+        const signal = this;
+        signal.emitter.on('valueChanged', (data) => {
+            listener(data.property, data.oldValue, data.newValue);
+        });
+    }
+    
+    subscribe(listener) {
+        return this.emitter.on('valueChanged', listener);
+    }
+    
+    unsubscribe(listener) {
+        this.emitter.off('valueChanged', listener);
+    }
+    
+    update(fn) {
+        const newValue = fn(this.value);
+        this.setValue(newValue);
+    }
+    
+    setValue(newValue) {
+        if (this.value !== newValue) {
+            const oldValue = this.value;
+            this.value = newValue;
+            this.emitter.emit('valueChanged', {oldValue, newValue});
+        }
+    }
 }
 
 const signal = (initialValue) => new CreateSignal(initialValue);
